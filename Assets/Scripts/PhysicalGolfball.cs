68b527380f7c4ba608ab70d59f65a6f03ce75c56
Clay Fowler <paul.clay.fowler@gmail.com>
2019-02-25 08:09:15 -0800

    Golfball input

--- Assets/Scripts/PhysicalGolfball.cs
+++ Assets/Scripts/PhysicalGolfball.cs
@@ -2,13 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PhysicalGolfball : MonoBehaviour
 {
     [SerializeField]
     private AudioSource _impactSound;
+
+    [SerializeField] 
+    private float _controlForce = 10f;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
     
     private void OnCollisionEnter(Collision c)
     {
         _impactSound.Play();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _rigidbody.AddForce(Vector3.forward * _controlForce);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            _rigidbody.AddForce(Vector3.back * _controlForce);            
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _rigidbody.AddForce(Vector3.left * _controlForce);            
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _rigidbody.AddForce(Vector3.right * _controlForce);                        
+        }
     }
 }
