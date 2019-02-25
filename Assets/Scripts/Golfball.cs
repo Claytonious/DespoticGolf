719692337b5a73542fede508a158d6b35cb2a50c
Clay Fowler <paul.clay.fowler@gmail.com>
2019-02-25 08:02:00 -0800

    Physical golfball

---
+++ Assets/Scripts/Golfball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golfball : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource _impactSound;
+    
+    private Vector3 _velocity;
+
+    private void Update()
+    {
+        _velocity.y -= 9.8f * Time.deltaTime;
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, _velocity.normalized, out hitInfo, _velocity.magnitude * Time.deltaTime, 1 << 9))
+        {
+            _impactSound.Play();
+            _velocity.y = 4f;
+        }
+        transform.position += _velocity * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PhysicalGolfball.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalGolfball : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource _impactSound;
+    
+    private void OnCollisionEnter(Collision c)
+    {
+        _impactSound.Play();
+    }
+}
