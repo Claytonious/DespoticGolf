f5f905faa4fa5f75aaf78bae253a8497192a2347
Clay Fowler <paul.clay.fowler@gmail.com>
2019-02-26 08:01:12 -0800

    Load from asset bundles

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
+using System.IO;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace DefaultNamespace
 {
@@ -10,13 +13,55 @@ namespace DefaultNamespace
         
         private IEnumerator Start()
         {
+            yield return LoadGolfball(prefab =>
+            {
+                if (prefab != null)
+                {
+                    _itemToSpawn = prefab.GetComponent<PhysicalGolfball>();
+                    Debug.Log("Loaded");
+                }
+            });
+            
             const int numToSpawn = 100;
             for (int i = 0; i < numToSpawn; i++)
             {
                 yield return new WaitForSeconds(Random.Range(0f, 0.2f));
                 var pos = transform.position + Random.onUnitSphere * Random.Range(0.1f, 0.5f);
-                Instantiate(_itemToSpawn, pos, Quaternion.identity);
+                Instantiate(_itemToSpawn.gameObject, pos, Quaternion.identity);
             }
+        }
+
+        private IEnumerator LoadGolfball(Action<GameObject> handler)
+        {
+            var loadRequest = AssetBundle.LoadFromFileAsync(
+                Path.Combine(Application.dataPath, "..", "AssetBundles", GetTargetName(), "golfballs"));
+            yield return loadRequest;
+            var assetBundle = loadRequest.assetBundle;
+            if (assetBundle == null)
+            {
+                Debug.LogError("Failed to load golf balls!");
+            }
+            else
+            {
+                var prefab = assetBundle.LoadAsset<GameObject>("PhysicalGolfball");
+                handler(prefab);
+                assetBundle.Unload(false);                
+            }
+        }
+
+        private string GetTargetName()
+        {
+#if UNITY_STANDALONE_OSX
+            return "StandaloneOSX";
+#elif UNITY_STANDALONE
+            return "StandaloneWindows64";
+#elif UNITY_IOS
+            return "iOS";
+#elif UNITY_ANDROID
+            return "Android";
+#else
+            return "Unknown";
+#endif
         }
     }
 }
